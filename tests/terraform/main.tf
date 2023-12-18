aaf1bf8f4ae292318e2d77041aedd3ff05093e37
Samuel Wibrow <15628653+swibrow@users.noreply.github.com>
2023-12-18 19:52:48 +0100

    add config lookup to workflows

--- tests/terraform/main.tf
+++ tests/terraform/main.tf
@@ -1,15 +1,8 @@
 resource "random_pet" "this" {
+  prefix = "${var.environment}-${var.region}-${var.github_repo}-"
   length = 2
 }
 
 resource "random_id" "this" {
   byte_length = 4
-}
-
-output "random_pet" {
-  value = random_pet.this.id
-}
-
-output "random_id" {
-  value = random_id.this.id
 }
---
+++ tests/terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "random_pet" {
+  value = random_pet.this.id
+}
+
+output "random_id" {
+  value = random_id.this.id
+}
